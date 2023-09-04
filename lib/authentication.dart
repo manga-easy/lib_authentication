308beb7927ed7155ecb30f750e44264cd3dd6a3d
lucas <lucas.cardo123@hotmail.com>
2023-09-04 18:18:11 -0400

    Update authentication.dart

--- lib/authentication.dart
+++ lib/authentication.dart
@@ -1,5 +1,7 @@
 library authentication;
 
 export 'src/auth/auth_service.dart';
+export 'src/auth/auth_appwrite_service.dart';
 export 'src/usecases/get_jwt_auth_case.dart';
+export 'src/usecases/get_jwt_auth_case_appwrite.dart';
 export 'src/exceptions/auth_exception.dart';
